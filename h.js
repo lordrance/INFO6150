b673040e5f9b45951ec39d9100e927dad4933385
uyrance <uyrance@hotmal.com>
2024-01-13 23:43:03 -0800

    Module 1 Assignment

---
+++ h.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: uyrance uyrance@hotmal.com
+ * @Date: 2024-01-12 19:45:57
+ * @LastEditors: uyrance uyrance@hotmal.com
+ * @LastEditTime: 2024-01-12 20:12:39
+ * @FilePath: \test\h.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('Hello world!');
